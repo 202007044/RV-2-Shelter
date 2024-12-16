f76c9c988bbd4a5fa5b642abed16fcb9a6c3432d
Bárbara <barbarafilipacarvalho08@gmail.com>
2024-12-16 15:45:16 +0000

    Som dos cães alterado

--- Assets/SoundPlayer.cs
+++ Assets/SoundPlayer.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class SoundPlayer : MonoBehaviour
 {
     private AudioSource audioSource;
-
+    
     void Start()
     {
         // Get the AudioSource component
         audioSource = GetComponent<AudioSource>();
+        audioSource.volume = 30.0f;
     }
 
     public void PlaySound()
